3d5cba48745426e256d854ecc408b806d2f9061f
Hirusha Rashmika <hirusharashmika3@gmail.com>
2025-08-07 16:40:06 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,14 +3,18 @@ const router = express.Router();
 const {
     getUsers,
     addUser,
+    loginUser,
     getUserById,
     getUserByEmail,
     updateUser,
     deleteUser
 } = require('../controllers/userController');
 
-router.get('/', getUsers);
+const verifyToken = require('../middleware/authMiddleware'); 
+
+router.get('/', verifyToken, getUsers); 
 router.post('/', addUser);
+router.post('/login', loginUser);
 router.get('/:id', getUserById);
 router.get('/email/:email', getUserByEmail);
 router.put('/:id', updateUser);
